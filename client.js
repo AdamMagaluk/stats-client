688b707de5d4536a5c01c76cfe88285c76cabb58
Adam Magaluk <AdamMagaluK@gmail.com>
2016-06-30 16:43:55 -0400

    Support telegraf

--- client.js
+++ client.js
@@ -2,11 +2,16 @@ var dgram = require('dgram');
 var util = require('util');
 var url = require('url');
 
-var Client = module.exports = function(hoststring, dimensions) {
+var Client = module.exports = function(hoststring, dimensions, options) {
+  if (!options) {
+    options = {};
+  }
+  
   var split = hoststring.split(':');
   this.host = split[0];
   this.port = Number(split[1]);
   this.dimensions = dimensions || {};
+  this.useTelegrafFormat = !!options.telegraf;
 
   // Create socket (ignore errors)
   this.socket = dgram.createSocket('udp4');
@@ -41,8 +46,23 @@ Client.prototype.send = function(bucket, value, dimensions) {
     }
   }
 
-  var key = url.format({ pathname: bucket, query: dimensions });
+  if (this.useTelegrafFormat) {
+    var key = bucket;
+    var tagKeys = Object.keys(dimensions);
+    if (tagKeys.length > 0) {
+      tagKeys.forEach(function(k) {
+        key += ',' + k + '=' + dimensions[k];
+      });
+    }
+  } else {
+    var key = url.format({ pathname: bucket, query: dimensions });
+  }
+
+  
+  
   var buffer = new Buffer(key + ':' + value);
+  console.log(buffer.toString())
+  
   // Send (ignore errors)
   this.socket.send(buffer, 0, buffer.length, this.port, this.host, function (err, bytes) {});
 };
--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 var Client = require('./client');
 
-var c = new Client('localhost:8125', { instance: 'i123' });
+var c = new Client('localhost:8125', { instance: 'i-123' }, { telegraf: true });
 
-c.increment('some.counter', { tenant: 'test' }); // -> some.counter?tenant=test&instance=i123:1|c
+setInterval(function() {
+  var t = Math.round(Math.random() * 10);
+  c.timing('some.timer', t, { tenant: 'test' }); // -> some.counter?tenant=test&instance=i123:1|c
+}, 200);
+setInterval(function() {
+  var t = Math.round(Math.random() * 10);
+  c.timing('some.timer', t,  { tenant: 'default' }); // -> some.counter?tenant=test&instance=i123:1|c
+}, 500);
+
